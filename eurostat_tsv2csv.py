d251ae27b05130944083e8d33e73b4e425bd4fea
GrigorisLionis <135837712+GrigorisLionis@users.noreply.github.com>
2023-09-10 09:29:48 +0300

    Update eurostat_tsv2csv.py
    comments added
    checks if file exists  before opening

--- eurostat_tsv2csv.py
+++ eurostat_tsv2csv.py
@@ -7,12 +7,19 @@ if(n==1):
     print("Error. Filename of tsv necessary")
     quit()
 filename=sys.argv[1]
-file1 = open( filename, 'r')
+if exists(filename):
+     tsv_file = open( filename, 'r')
+else:
+     print("Error. Filename does not exist")
+     quit()
+    
 filename_components=filename.split(".")
 table_name=filename_components[0]
 csv_filename=table_name+".csv"
 csv_file=open(csv_filename,"w")
-Lines = file1.readlines()
+sql_filename=table_name+".sql"
+sql_file=open(sql_filename,"w")
+Lines = tsv_file.readlines()
 head=Lines[0]
 head_br=head.split("\t")
 
@@ -20,19 +27,16 @@ for i in range(0,len(head_br)):
     head_br[i]=(head_br[i]).strip()
 
 variables=head_br[0].split(",")
-head_br.pop(0)
-len_of_time=max(map(len,head_br))
+head_br.pop(0) #head br contains now only time columns
+len_of_time=max(map(len,head_br))  # find max len of time column.
+
 last_entry=variables[len(variables)-1]
 last_variables=last_entry.split("\\")
-#print(head_br)
-#print(variables)
-#print(last_entry)
 
 if(last_variables[1]!="time"):
-    print("TSV wrong format")
+    print("Error.TSV wrong format")  #internal check of TSV structure
     quit()
 variables=variables[0:len(variables)-1]
-len_of_fields=[0]*len(variables)
 variables.append(last_variables[0])
 len_of_fields=[0]*len(variables)
 #count size of varchar data, to construct sql table
@@ -43,19 +47,20 @@ variables.append("flags")
 for i in range(0,len(variables)-1):
     print(variables[i].strip(),",",end="",sep="",file=csv_file)
 print(variables[len(variables)-1],file=csv_file)
-
+#csv header
 
 Lines.pop(0)
 for line in Lines:
-    data=line.split("\t")
-    row_vars=data.pop(0)
+    data=line.split("\t")  #split rown on tab
+    row_vars=data.pop(0)   
     row_vars_const=row_vars.split(",")
     for i in range(0,len(row_vars_const)):
          if len(row_vars_const[i])>len_of_fields[i]:
                 len_of_fields[i]=len(row_vars_const[i])
-    #print(row_vars)
+    #check all rows to find length of fields
+    
     for i in range(0,len(data)):
-           val=data[i].strip()
+           val=data[i].strip() 
            flags="NULL"
            if(val==":"):
                 val="NULL"
@@ -66,19 +71,21 @@ for line in Lines:
                 for j in range(1,len(val_seg)):
                       flags=flags+val_seg[j]
            print(row_vars,",",head_br[i],",",val,",",flags,end="\n",sep="",file=csv_file)
-#print(len_of_time)
-#print(len_of_fields)
 
 def sql_string(var_name,var_length):
     return(var_name+" VARCHAR("+str(var_length+5)+"),")
-
-
+    
+#SQL SCHEMA
+#table name is the same as in tsv file
+#all fields are varchar, with approrpiatte length= MAX(length of field in data+5)
+#except of field value which is FLOAT
+#we keep also a field FLAGS, as in some cases Eurostat assigns to data a (c or b) field
 sql_string_vars=""
 for i in range(0,len(variables)-3):
     sql_string_vars=sql_string_vars+sql_string(variables[i],len_of_fields[i]) 
 sql_string_vars="CREATE TABLE "+table_name+" ("+sql_string_vars
 sql_string_vars=sql_string_vars+sql_string("obs_time",len_of_time)
 sql_string_vars=sql_string_vars+"value FLOAT,flags VARCHAR(6) );"
-print(sql_string_vars)
+print(sql_string_vars,file=sql_file)
     
 
