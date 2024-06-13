ed10204c93093516ffba931c0d29df90168f16d9
GRIGORIS_L <glionis@kke.gr>
2024-06-13 13:09:24 +0300

    Eliminated shell script, more organized code

---
+++ eurostat2csv.py
@@ -0,0 +1,26 @@
+import sys
+from os.path import exists
+from os import getcwd
+import requests 
+import gzip
+from eurostat_get_tsv import eurostat_get_tsv
+from eurostat_tsv2csv import eurostat_tsv2csv
+
+n = len(sys.argv)
+
+if(n==1):
+    print("Error. Table name necessary")
+    sys.exit(-1)
+
+table_name=sys.argv[1]
+return1=eurostat_get_tsv(table_name)
+if(return1!=1):
+   print("Error reading tsv from database.Error ",return1)
+
+tsv_file=table_name+".tsv"
+return2=eurostat_tsv2csv(tsv_file)
+if(return2!=1):
+   print("Error decoding tsv file")
+print("csv,sql files ready")
+print("To import use: mysql -e \"source ./"+table_name+".sql\"")
+
